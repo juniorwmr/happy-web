9137f47b1088a3e5359c88dc3affdc45a9b53067
Washington Muniz <juniorripardo@gmail.com>
2020-10-13 14:26:55 -0300

    :art: adjusting logo's alt

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -9,7 +9,7 @@ const Landing: React.FC = () => {
   return (
     <div id="page-landing">
       <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
+        <img src={logoImg} alt="Happy Logo" />
         <main>
           <h1>Leve felicidade para o mundo</h1>
           <p>Visite orfanatos e mude o dia de muitas crianças.</p>
