72d0a3d49d36067b050492863d52ec4282a30f37
Washington Muniz <juniorripardo@gmail.com>
2020-10-17 02:25:28 -0500

    Integração com API

--- src/pages/Auth/CreateUser.tsx
+++ src/pages/Auth/CreateUser.tsx
@@ -1,29 +1,75 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import Sidebar from '../../components/Sidebar';
+import { api } from '../../services/api';
 
 import '../../styles/pages/create-user.css';
 
 const CreateUser: React.FC = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmitRegister(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      name,
+      email,
+      password,
+    };
+
+    const response = await api.post('/users', data);
+    if (response.status === 200) {
+      alert('Cadastro efetuado com sucesso!');
+    }
+  }
+
   return (
     <div id="page-create-user">
       <Sidebar />
       <main>
-        <form className="create-user-form">
+        <form
+          className="create-user-form"
+          onSubmit={(event) => handleSubmitRegister(event)}
+        >
           <fieldset>
             <div className="legend">
               <h1>Cadastrar Usuário</h1>
             </div>
             <div className="input-block">
               <label htmlFor="name">Nome</label>
-              <input id="name" name="name" onChange={() => {}} />
+              <input
+                autoFocus
+                required
+                type="text"
+                id="name"
+                name="name"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
             </div>
             <div className="input-block">
               <label htmlFor="email">E-mail</label>
-              <input id="email" name="email" onChange={() => {}} />
+              <input
+                type="email"
+                required
+                id="email"
+                name="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
             </div>
             <div className="input-block">
               <label htmlFor="password">Senha</label>
-              <input id="password" name="password" onChange={() => {}} />
+              <input
+                type="password"
+                minLength={8}
+                required
+                id="password"
+                name="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
             </div>
             <button className="register-button" type="submit">
               Cadastrar
--- src/pages/Auth/SignIn.tsx
+++ src/pages/Auth/SignIn.tsx
@@ -1,31 +1,68 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 
 import Sidebar from '../../components/Sidebar';
 import { FiMail, FiKey } from 'react-icons/fi';
 
 import '../../styles/pages/signin.css';
 import { Link } from 'react-router-dom';
+import { api } from '../../services/api';
 
 const SignIn: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSignInForm(event: FormEvent) {
+    event.preventDefault();
+
+    if (email && password) {
+      const response = await api.post('/auth', {
+        email,
+        password,
+      });
+      if (response.data.auth) {
+        alert('Login efetuado com sucesso!');
+      }
+    }
+  }
+
   return (
     <div id="page-sign-in">
       <Sidebar />
       <main>
-        <form className="sign-in-form">
+        <form
+          className="sign-in-form"
+          onSubmit={(event) => handleSignInForm(event)}
+        >
           <fieldset>
             <div className="legend">
               <h1>Acesso Restrito</h1>
             </div>
 
             <div className="input-block">
-              <label htmlFor="name">E-mail</label>
+              <label htmlFor="email">E-mail</label>
               <FiMail size={20} color="#6e6e6e" className="icon key-icon" />
-              <input id="email" name="email" onChange={() => {}} />
+              <input
+                autoFocus
+                type="email"
+                required
+                id="email"
+                name="email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
             </div>
             <div className="input-block">
-              <label htmlFor="name">Senha</label>
+              <label htmlFor="password">Senha</label>
               <FiKey size={20} color="#6e6e6e" className="icon key-icon" />
-              <input id="password" name="password" onChange={() => {}} />
+              <input
+                type="password"
+                minLength={8}
+                required
+                id="password"
+                name="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
             </div>
             <div className="forget_passowrd-area">
               <Link to="/forget_password" className="forget_password">
