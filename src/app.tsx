c680912046d76a5997cd91182c0d0911f8d89421
Washington Muniz <juniorripardo@gmail.com>
2020-10-17 22:20:09 -0500

    authenticate context provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-import Routes from './routes';
+import Routes from './routes/routes';
+
+import { AuthProvider } from './context/auth';
 
 import './styles/global.css';
 import 'leaflet/dist/leaflet.css';
 
 const App: React.FC = () => {
-  return <Routes />;
+  return (
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
+  );
 };
 
 export default App;
